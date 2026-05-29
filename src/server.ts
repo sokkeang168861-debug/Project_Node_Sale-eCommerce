42991b347bd20d89872f5086646312941c57de6a
timtola102-oss <timtola102@gmail.com>
2026-05-29 08:59:42 +0700

    fix

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,17 @@ import Database from "./config/db.js";
 const PORT = 3000;
 
 async function startServer() {
+    try {
+        await Database.getInstance().getPool();
 
-    await Database.getInstance().connect();
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
 
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+    } catch (error) {
+        console.error("Database connection failed:", error);
+        process.exit(1);
+    }
 }
 
 startServer();
